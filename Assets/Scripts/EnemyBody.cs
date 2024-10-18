fe17700ba087da32c4524fdc1e481004f86e772b
Airi0729 <h1202311009@hiratagakuen.onmicrosoft.com>
2024-10-18 19:58:41 +0900

    enemy 追加

--- Assets/Scripts/EnemyBody.cs
+++ Assets/Scripts/EnemyBody.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class EnemyBody : MonoBehaviour
 {
-    public GameObject player;//playerの取得
+    public Transform player_pos;//player位置座標の取得
     public GravityAttractor attractor;//GravityAttractor.csを参照
+    public float movespeed = 0.5f;//移動速度
     private Transform mytransform;
     private Rigidbody rb;
+    private bool trigger = true;//true 動き続ける false プレイヤーの前にいる(停止状態)
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +23,23 @@ public class EnemyBody : MonoBehaviour
     {
         //GravityAttractor.csのAttract関数処理
         attractor.Attract(mytransform, rb);//transformとrigidbodyの情報を渡す
+    }
 
-        //playerへの追従
-
+    private void FixedUpdate()
+    {
+        if (trigger == true)
+            mytransform.position = Vector3.Lerp(mytransform.position, player_pos.position, movespeed * Time.deltaTime);//プレイヤーの位置移動
     }
 
     //オブジェクト同士が接触した時
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Player") Destroy(player);
+        if (collision.gameObject.name == "Player") trigger = false;
+    }
+
+    //オブジェクト同士が離れた時
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Player") trigger = true;
     }
 }
