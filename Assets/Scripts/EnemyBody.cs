b51c1f022e34795a5f5156b145f995f3d0bfba6f
Airi0729 <h1202311009@hiratagakuen.onmicrosoft.com>
2024-10-18 20:30:30 +0900

    エネミーとプレイヤーの衝突判定作り直した。

--- Assets/Scripts/EnemyBody.cs
+++ Assets/Scripts/EnemyBody.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class EnemyBody : MonoBehaviour
 {
-    public GameObject player;//playerの取得
+    public Transform player;//playerの取得
     public GravityAttractor attractor;//GravityAttractor.csを参照
-    private Transform mytransform;
+    public float movespeed = 0.5f;
+    Transform mytransform;
     private Rigidbody rb;
+    private bool trigger = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,14 +23,22 @@ public class EnemyBody : MonoBehaviour
     {
         //GravityAttractor.csのAttract関数処理
         attractor.Attract(mytransform, rb);//transformとrigidbodyの情報を渡す
-
-        //playerへの追従
-
     }
 
-    //オブジェクト同士が接触した時
-    void OnCollisionEnter(Collision collision)
+    private void FixedUpdate()
     {
-        if (collision.gameObject.name == "Player") Destroy(player);
+        if(trigger == false)
+            mytransform.position = Vector3.Lerp(mytransform.position, player.position, movespeed * Time.deltaTime);
+    }
+    //オブジェクト同士が接触した時
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player") trigger = true;
+    }
+
+    //オブジェクト同士が離れた場合
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Player") trigger = false;
     }
 }
