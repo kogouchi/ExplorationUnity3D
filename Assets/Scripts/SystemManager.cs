2e7806a3f7fc3302352016f3efd66826254e0ab2
DESKTOP-M30FNO0\GA2A <aaa>
2024-11-28 15:30:55 +0900

    Clear状態からマップに移行のキー追加。 GameOver時のリザルトキー追加。

--- Assets/Scripts/SystemManager.cs
+++ Assets/Scripts/SystemManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;//シーンで使用
 
 public class SystemManager : MonoBehaviour
 {
+    public GameObject clearText;//Clearテキスト
+    public GameObject endText;//Endテキスト
+    public AudioSource audioSource;//流すための音源を入れるもの
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +18,33 @@ public class SystemManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(clearText.activeInHierarchy)
+        {
+            audioSource.Stop();//オーディオソースの停止
+            if(Input.GetKeyDown(KeyCode.Space))
+                SceneManager.LoadScene("MapScene");//MapSceneに切り替え
+        }
+
+        if (endText.activeInHierarchy)
+        {
+            audioSource.Stop();//オーディオソースの停止
+            if (Input.GetKeyDown(KeyCode.Space))
+                SceneManager.LoadScene("MapScene");//MapSceneに切り替え
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                if (SceneManager.GetActiveScene().name == "GameScene1")
+                    SceneManager.LoadScene("GameScene1");//GameScene1に切り替え
+                if (SceneManager.GetActiveScene().name == "GameScene2")
+                    SceneManager.LoadScene("GameScene2");//GameScene2に切り替え
+                if (SceneManager.GetActiveScene().name == "GameScene3")
+                    SceneManager.LoadScene("GameScene3");//GameScene3に切り替え
+                if (SceneManager.GetActiveScene().name == "GameScene4")
+                    SceneManager.LoadScene("GameScene4");//GameScene4に切り替え
+            }
+        }
+
         //F1キーが押された場合
         if (Input.GetKeyDown(KeyCode.F1))
-        {
             SceneManager.LoadScene("TitleScene");//TitleSceneに切り替え
-        }
     }
 }
