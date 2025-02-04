0656f6a39d7db729abc85e97efcd4927e83a64b4
DESKTOP-3GUQ93R\GA2A <a>
2025-02-04 12:33:48 +0900

    タイトル戻るボタンを設定画面に変更。 ただし、ステージ１のみ切り替え可能。 ボタンを一番上にするとセレクトされなくなるバグあり

--- Assets/Scripts/SystemManager.cs
+++ Assets/Scripts/SystemManager.cs
@@ -1,31 +1,44 @@
+using Palmmedia.ReportGenerator.Core;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;//シーンで使用
+using UnityEngine.UI;//テキスト表示で使用
 
 public class SystemManager : MonoBehaviour
 {
     public GameObject clearText;//Clearテキスト
-    public GameObject endText;//Endテキスト
+    public GameObject GameoverText;//GameOvewテキスト
+    public GameObject settingText;//Settingテキスト
     public AudioSource audioSource;//流すための音源を入れるもの
+    public AudioClip[] audioClips;//音源格納
+    public Button playbutton;//続けるボタン
+    public Button restartbutton;//リトライボタン
+    public Button endbutton;//endボタン
+
+    private int buttonflg = 0;//各ボタンの番号振り分けフラグ(複数なのでint)
+    private bool keyflg = false;//ボタンが押されているかどうかフラグ
 
     // Start is called before the first frame update
     void Start()
     {
-
+        settingText.SetActive(false);//設定画面非表示
+        playbutton.Select();//始めに選択されている
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(clearText.activeInHierarchy)
+        SettingManager();
+        SelectChange();
+        if (clearText.activeInHierarchy)
         {
             audioSource.Stop();//オーディオソースの停止
             if(Input.GetKeyDown(KeyCode.Space))
                 SceneManager.LoadScene("MapScene");//MapSceneに切り替え
         }
-
-        if (endText.activeInHierarchy)
+        if (GameoverText.activeInHierarchy)
         {
             audioSource.Stop();//オーディオソースの停止
             //if (Input.GetKeyDown(KeyCode.Space))
@@ -42,11 +55,83 @@ public class SystemManager : MonoBehaviour
                     SceneManager.LoadScene("GameScene4");//GameScene4に切り替え
             }
         }
+    }
 
-        //F1キーが押された場合
+    /// <summary>
+    /// 設定画面の表示
+    /// </summary>
+    void SettingManager()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene("TitleScene");//TitleSceneに切り替え
+            settingText.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+    
+    /// <summary>
+    /// ボタンセレクト
+    /// </summary>
+    void SelectChange()
+    {
+        //キーが離された場合
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.W)) keyflg = false;
+
+        //Sキーが押された場合(flgでボタンの位置を変更+flgが4だった場合何もしない)
+        if (Input.GetKeyDown(KeyCode.S) && keyflg == false)
+        {
+            keyflg = true;
+            switch (buttonflg)
+            {
+                case 0:
+                    restartbutton.Select();//フォーカス変更
+                    audioSource.PlayOneShot(audioClips[1]);
+                    restartbutton.interactable = true;
+                    buttonflg = 1;
+                    break;
+                case 1:
+                    endbutton.Select();//フォーカス変更
+                    audioSource.PlayOneShot(audioClips[1]);
+                    endbutton.interactable = true;
+                    buttonflg = 2;
+                    break;
+            }
+        }
+
+        //Wキーが押された場合
+        if (Input.GetKeyDown(KeyCode.W) && keyflg == false)
+        {
+            keyflg = true;
+            switch (buttonflg)
+            {
+                case 2:
+                    restartbutton.Select();//フォーカス変更
+                    audioSource.PlayOneShot(audioClips[1]);
+                    restartbutton.interactable = true;
+                    buttonflg = 1;
+                    break;
+                case 1:
+                    playbutton.Select();//フォーカス変更
+                    audioSource.PlayOneShot(audioClips[1]);
+                    playbutton.interactable = true;
+                    buttonflg = 0;
+                    break;
+            }
+        }
+
+        //スペースキーが押された場合
+        if (Input.GetKeyDown(KeyCode.Space) && keyflg == false)
+        {
+            keyflg = true;
+            audioSource.PlayOneShot(audioClips[0]);
+            if (buttonflg == 0)
+            {
+                playbutton.Select();//選択ボタンリセットしておく
+                settingText.SetActive(false);
+                Time.timeScale = 1.0f;
+            }
+            if (buttonflg == 1) SceneManager.LoadScene(SceneManager.GetActiveScene().name);//現在のシーン再ロード
+            if (buttonflg == 2) SceneManager.LoadScene("TitleScene");//TitleSceneに切り替え
         }
     }
 }
